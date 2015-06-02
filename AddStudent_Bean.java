0b2bb473db75a1bfd51fbbf26f50b3e0d8f8d82f
cjxwd <18463101562@163.com>
2015-06-02 11:33:02 +0800

    SoftwareProject
    软件工程项目

---
+++ AddStudent_Bean.java
@@ -0,0 +1,33 @@
+
+public class AddStudent_Bean {
+	
+	private int studentID;
+    private String studentName;
+    private int classID;
+    
+    public int getStudentID()
+    {
+        return studentID;
+    }
+    public String getStudentName()
+    {
+        return studentName;
+    }
+    public int getClassID()
+    {
+        return classID;
+    }
+    public void setStudentID(int studentID)
+    {
+        this.studentID = studentID;
+    }
+    public void setStudentName(String studentName)
+    {
+        this.studentName = studentName;
+    }
+    public void setClassID(int classID)
+    {
+        this.classID = classID;
+    }
+
+}
